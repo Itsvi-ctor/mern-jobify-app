b2ece2dcea42bcf7ca5569b6ce2484b74df57622
Olaniyan Oluwafolajimi Opemiposi <folajimiolaniyan@gmail.com>
2024-07-11 23:42:22 +0100

    public file

--- server.js
+++ server.js
@@ -47,10 +47,10 @@ app.use('/api/v1/jobs', authenticateUser, jobRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/auth', authRouter);
 
-app.use(express.static(path.resolve(__dirname, "./client/dist")));
+// app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
 });
 
 app.use('*', (req, res) => {
