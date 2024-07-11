353d45d8ccd27856dc27e8e7e238f5eb468517ff
Olaniyan Oluwafolajimi Opemiposi <folajimiolaniyan@gmail.com>
2024-07-11 20:45:23 +0100

    port change

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ app.use('*', (req, res) => {
 
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 5100;
+const port = process.env.PORT || 5200;
 
 try {
   await mongoose.connect(process.env.MONGO_URL);
