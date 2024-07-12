35ed811b4f87e84e6b2864719614d72cc3e64d9f
Olaniyan Oluwafolajimi Opemiposi <folajimiolaniyan@gmail.com>
2024-07-12 14:49:54 +0100

    description change

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -10,25 +10,28 @@ const Landing = () => {
       <nav>
         <Logo />
       </nav>
-      <div className='container page'>
-        <div className='info'>
+      <div className="container page">
+        <div className="info">
           <h1>
             job <span>tracking</span> app
           </h1>
           <p>
-            I'm baby wayfarers hoodie next level taiyaki brooklyn cliche blue
-            bottle single-origin coffee chia. Aesthetic post-ironic venmo,
-            quinoa lo-fi tote bag adaptogen everyday carry meggings +1 brunch
-            narwhal.
+            Jobify is a user-friendly app designed to help individuals manage
+            and track their job applications seamlessly. With Jobify, users can
+            organize their job search, monitor application statuses, and set
+            reminders for follow-ups. The intuitive interface allows for easy
+            input of job details, company information, and interview schedules.
+            Stay on top of your job hunt and streamline your application process
+            with Jobify.
           </p>
-          <Link to='/register' className='btn register-link'>
+          <Link to="/register" className="btn register-link">
             Register
           </Link>
-          <Link to='/login' className='btn '>
+          <Link to="/login" className="btn ">
             Login / Demo User
           </Link>
         </div>
-        <img src={main} alt='job hunt' className='img main-img' />
+        <img src={main} alt="job hunt" className="img main-img" />
       </div>
     </Wrapper>
   );
